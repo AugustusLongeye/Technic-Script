758800aaeb1825d8fb87836e2248d9d9ece7b90b
Augustus Longeye <augustus.longeye@gmail.com>
2015-08-01 07:05:07 -0400

    Added all file work methods to Folder, added all builtin representation methods.

--- folder.py
+++ folder.py
@@ -4,44 +4,54 @@ class Folder(object):
 
     Folder holds a main path with an optional
     list of subdirs. Path is checked and touched
-    before use. Includes methods to deal with
+    at init. Includes methods to deal with
     contents of self in relation to other Folder
     objects, such as copying or wiping self.
-    
-    Maintains an internal list of all obejects
-    in own path as a generator to use when needed.
     """
     
     import os
     import shutil
-    from exceptions import IllegalArgError
-    from exceptions import InvalidPathError
+    from distutils import dir_util
+    from exception import IllegalArgError
+    from exception import InvalidPathError
 
     path = None
     subdirs = {}
-    illegal_chars = ";*?<>|()\"\'"
-    last_err = None
-    contents = ["""GENERATOR"""]
+    __illegal_chars = ";*?<>|()\"\'"
+    __last_err = None
     
     def __init__(self, path):
         """
-        Set path var, check for illegal chars.
+        Set path var, check for illegal chars,
+        touch path to varify access rights.
+        
+        Raise InvalidPathError if no access.
         """
-        for char in self.illegal_chars:
+        for char in self.__illegal_chars:
             if char in path:
                 raise IllegalArgError(char, path)
-        self.path = path
-
-    def touch(self):
-        """
-        Test path location is read/writeable.
-        
-        Raise InvalidPathError if not.
-        """
+        if path.endswith("/"):
+            self.path = path
+        else:
+            self.path = path + "/"
         try:
-            self.os.makedirs(self.path)
+            self.touch()
+        except InvalidPathError as e:
+            raise e
+
+    def touch(self, path=None):
+        """
+        Test path location is read/writeable,
+        if path=None; test self.path.
+        
+        Raise InvalidPathError if cannot access.
+        """
+        if not path:
+            path = self.path
+        try:
+            self.os.makedirs(path)
         except OSError as e:
-            if not self.os.path.isdir(self.path):
+            if not self.os.path.isdir(path):
                 raise self.InvalidPathError(path, e)
         else:
             return True
@@ -83,7 +93,58 @@ class Folder(object):
         elif isinstance(other, str):
             return Folder(self.path + other,
                            subdirs=self.subdirs)
+    
+    # Methods to alter content
+    def copy(self, dst, log=True):
+        """
+        Copy contents of self to dst recursively.
+        If log, return list of files copied.
+        """
+        to_log = self.dir_util.copy_tree(repr(self), dst)
+        if log:
+            return to_log
+        else:
+            return
+
+    def move(self, dst, log=True):
+        """
+        Copy contents of self to dst, then
+        wipe self. If log, return list of
+        files moved.
+        """
+        to_log = self.copy(dst)
+        self.wipe()
+        if log:
+            return to_log
+        else:
+            return
         
+    def zip(self, archive_name, dst=None):
+        """
+        Zip self.path contents into name.zip.
+        If dst is a path, zip file will be put there.
+        Else zip will be put in self.path.
+        """
+        if not dst:
+            archive_loc = self.path + archive_name
+        else:
+            archive_loc = dst + archive_name
+        self.os.chdir(self.path)
+        self.shutil.make_archive(archive_loc, "zip", self.path)
+        
+    def wipe(self, reinit=True):
+        """
+        Wipe self, removes all contents recursively.
+        If reinit, retouch path when done.
+        """
+        self.shutil.rmtree(self.path, ignore_errors = True)
+        if reinit:
+            try:
+                return self.touch()
+            except InvalidPathError as e:
+                raise e
+    
+    # Representation Methods
     def print_state(self):
         """
         Return list of current class state.
@@ -103,46 +164,27 @@ class Folder(object):
         else:
             to_log += "No Sub-Directories found."
         to_log += "-----"
+        if not __last_err:
+            to_log += "Last Error:"
+            to_log += self.__last_err
+        to_log += "-----"
         to_log += "Folder Instance- End Dump"
         to_log += "==================="
-        return to_log
+        return to_log      
    
-    def copy(self, dst, dupes="ignore"):
-        #copy contents of self to dst
-        #overwright/rename/ignore
-        
-    def move(self, dst, dupes="ignore"):
-        #move contents of self to dst
-        #overwright/rename
-        
-    def zip(self, name, dst=None):
-        """
-        Zip self.path contents into name.zip.
-        If dst is a path, zip file will be put there.
-        Else zip will be put in self.path.
-        """
-        if not dst:
-            name = self.path + name
+    def __str__(self):
+        return self.path
+    
+    def __repr__(self):
+        return self.path
+    
+    def __unicode__(self):
+        return self.path
+    
+    def __bool__(self):
+        try:
+            self.touch()
+        except self.InvalidPathError as e:
+            return False
         else:
-            name = dst + name
-        os.chdir(self.path)
-        shutil.make_archive(name, "zip", self.path)
-        
-    def wipe(self, reinit=True):
-        """
-        Wipe self, removes all contents recursively.
-        
-        Optional "reinit=True" param to retouch the
-        folder when finished.
-        """
-        shutil.rmtree(self.path, ignore_errors = True)
-        if reinit:
-            try:
-                return self.touch()
-            except InvalidPathError as e
-                raise e
-        
-    def sync(self, dst):
-        #only copy changed/no present contents
-        #of self to dst. All same ignored.
-        pass
+            return True
--- logger.py
+++ logger.py
@@ -20,20 +20,38 @@ class Logger(object):
              "verbose":3,
              "debug":4}
     
-    current_log_level = ""
+    __current_log_level = ""
     script_location = ""
-    stack = []
+    __stack = []
+    __to_file = False
+    __bulk_log = False
+    __log_file = ""
     
-    def __init__(self, current_level = 2):
+    def __init__(self, current_level = 2, output="terminal",
+                 output_style="live"):
         """
         Set params, get location of self.
+        
+        current_level can be 0 for no logging, up
+        to 4 for debug logging.
+        
+        output is terminal or file.
+        
+        output_style can be live or bulk. Live
+        will print in real time, bulk will wait
+        until termination and print all content.
         """
-        self.current_log_level = current_level
+        if output == "file":
+            self.__to_file = True
+        if output_style == "bulk":
+            self.__bulk_log = True
+        self.__current_log_level = current_level
         self.__call__("Logger initialised.", self.level["debug"])
         self.__call__("Logger level set at Debug", self.level["debug"])
         self.script_location = self.getcwd
         self.__call__("Script location: {0}".format(self.script_location),
                       self.level["debug"])
+        self.__log_file = self.script_location + "/log.txt"
     
     def __call__(self, message = "", level=level["normal"]):
         """
@@ -41,36 +59,67 @@ class Logger(object):
         than global logging level. Also 
         add all messages to stack.
         """
-        
-        #TODO log to file/console
-        
-        self.stack.append(message)
-        if level <= self.current_log_level:
-            print(message)
+        if not message:
+            return
+        if hasattr(message, "__iter__"):
+        # Will intentionally fail for str, only want to iter
+        # on lists or the like, not strings.
+            for line in message:
+                self.__call__(message, level)
+        self.__stack.append(message)
+        if level <= self.__current_log_level:
+            if not self.__bulk_log:
+                if self.__to_file:
+                    self.__write(message)
+                else:
+                    print(message)
             
     def log(*args, **kwargs):
         """
-        You wanna log with logger.log()
-        rather than logger()?!
+        You wanna log with log.log()
+        rather than log()?!
         I ain't gonna' stop you!
         
         Wrapper for __call__.
         """
         self.__call__(args, kwargs)
+        
+    def __write(self, message):
+        """
+        Write given line to log file.
+        Write safely such that file is
+        opened/closed after each operation.
+        
+        To write bulk data use _bulk_write
+        """
+        with open(self.__log_file, "w") as log_file:
+            pass
+        
+    def __bulk_write(self):
+        """
+        Write list to file as single bulk.
+        
+        Accept list and print line-by-line.
+        Used to shutn IO time to end of script
+        rather than during runtime.
+        """
+        with open(self.__log_file, "w") as log_file:
+            for line in self.__stack:
+                log_file.write(line)
             
     def error(self, message, exception=""):
         """
         Log exception and dumps current
         stack to file.
         """
-        self.stack.append("ERROR: " + message)
+        self.__stack.append("ERROR: " + message)
         if exception:
-            self.stack.append("EXCEPTION: " + exception)
-        self.__call__("Stack saved to " + repr(dump_path), 
+            self.__stack.append("EXCEPTION: " + exception)
+        self.__call__("Stack saved to " + self.__log_file, 
                       self.level["silent"])
         self.dump_stack(out="file")
 
-    def dump_stack(self, out="file", path=""):
+    def dump_stack(self, out="file"):
         """
         Dump current stack to console or file, 
         will default to dumping to file.
@@ -81,7 +130,7 @@ class Logger(object):
         file.
         """
         if out is "terminal":
-            print(self.stack)
+            print(self.__stack)
         if out is "file":
             from datetime import datetime as time
             now = time.now().strftime("%Y-%m-%d_%H.%M.%s")
@@ -90,8 +139,8 @@ class Logger(object):
             else:
                 path = script_location + "dump.{0}.txt".format(now)
             try:
-                with open(path, "w") as file:
-                    for line in self.stack:
+                with open(self.__log_file, "w") as file:
+                    for line in self.__stack:
                         file.write(line + "\n")
             except IOError as e:
                 raise e
@@ -100,4 +149,4 @@ class Logger(object):
         """
         Set logger's current log level.
         """
-        self.current_log_level = level
+        self.__current_log_level = level
